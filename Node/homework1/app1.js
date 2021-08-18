6c55d773a8f65db1a8fecefee5e5fc19f88ab542
MyroslavaNY <MyroslavaNY@gmail.com>
2021-08-18 14:23:42 +0300

    num1

--- Node/homework1/app1.js
+++ Node/homework1/app1.js
@@ -1,29 +1,45 @@
 const fs = require('fs');
 const path = require('path');
-const elenPath = path.join(__dirname, '1800', 'elen.json');
-const romanPath = path.join(__dirname, '1800','roman.json');
-const robertPath= path.join(__dirname, '1800', 'robert.json');
-const viraPath = path.join(__dirname, '2000', 'vira.json');
-const nazarPath = path.join(__dirname, '2000', 'nazar.json');
-const daraPath =path.join(__dirname, '2000', 'dara.json');
 
 
-fs.writeFile(elenPath, '',err => {
+
+const elenPath = path.join(__dirname, 'people', 'elen.json');
+const romanPath = path.join(__dirname, 'people','roman.json');
+const robertPath= path.join(__dirname, 'people', 'robert.json');
+const viraPath = path.join(__dirname, 'people', 'vira.json');
+const nazarPath = path.join(__dirname, 'people', 'nazar.json');
+const daraPath =path.join(__dirname, 'people', 'dara.json');
+
+const femalePath = path.join(__dirname, 'went','2000');
+const malePath = path.join(__dirname,'went','1800') ;
+
+const allPath = path.join(__dirname, 'people');
+
+
+fs.mkdir(femalePath, {recursive:true},err =>{
+    console.log(err);
+} );
+fs.mkdir(malePath,{recursive:true}, err => {
+    console.log(err);
+});
+
+
+fs.writeFile(elenPath, '{"name": "Elen", "gender": "female"}',err => {
 
 });
-fs.writeFile(romanPath,'', err => {
+fs.writeFile(romanPath,'{"name": "Roman", "gender": "male"}', err => {
 
 });
-fs.writeFile(robertPath, '', err => {
+fs.writeFile(robertPath, '{"name": "Robert", "gender": "male"}', err => {
 
 });
-fs.writeFile(viraPath, '', err => {
+fs.writeFile(viraPath, '{"name": "Vira", "gender": "female"}', err => {
 
 });
-fs.writeFile(nazarPath, '', err => {
+fs.writeFile(nazarPath, '{"name": "Nazar", "gender": "male"}', err => {
 
 });
-fs.writeFile(daraPath, '', err => {
+fs.writeFile(daraPath, '{"name": "Dara", "gender": "female"}', err => {
 
 });
 
@@ -33,5 +49,78 @@ fs.readFile(elenPath, (err,data)=> {
         return;
     }
     console.log(data.toString());
-})
+});
+fs.readFile(robertPath, (err,data)=> {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+fs.readFile(romanPath, (err,data)=> {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+fs.readFile(daraPath, (err,data)=> {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+fs.readFile(nazarPath, (err,data)=> {
+       if (err){
+        console.log(err);
+        return;
+    }
+        console.log(data.toString())
+});
+fs.readFile(viraPath, (err,data)=> {
+    if (err){
+        console.log(err);
+        return;
+    }
+       console.log(data.toString());
+});
+
+fs.readdir(allPath,((err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(files);
+
+    files.forEach(file =>{
+        const filePath = path.join(allPath, file);
+        fs.readFile(filePath,((err1, data) => {
+            if (err1){
+                console.log(err1);
+                return;
+            }
+            console.log(JSON.parse(data.toString()));
+
+            const persone = data.toString();
+            console.log(persone);
+            if(persone.includes("female")){
+                fs.rename(allPath, femalePath, err2 => {
+                    console.log(err2);
+
+                })
+            }
+            if (persone.includes("male")){
+                fs.rename(allPath,malePath, err2 => {
+                    console.log(err2);
+                })
+            }
+        }))
+    });
+}))
+
+
+
+
+
 
